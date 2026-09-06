a788ece2c38f3a72c0fcb97f8bfe0a9637b35a36
IALikhon <likhon9112@gmail.com>
2026-09-06 20:10:50 +0600

    Refactor code structure for improved readability and maintainability

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { ScrollTrigger, SplitText } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl text-white">Hello</h1>
+    </div>
+  )
+}
+
+export default App;
