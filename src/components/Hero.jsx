dfec0cbf9ff8f6913b0f2c671498f12cb98eff63
IALikhon <likhon9112@gmail.com>
2026-09-08 20:37:39 +0600

    feat: enhance Hero component with video playback and responsive animations

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,13 @@
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { SplitText } from "gsap/all";
+import { useRef } from "react";
+import { useMediaQuery } from "react-responsive";
 
 const Hero = () => {
+  const videoRef = useRef();
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+
   useGSAP(() => {
     const heroSplit = new SplitText(".title", { type: "chars, words" });
 
@@ -37,11 +42,30 @@ const Hero = () => {
       })
       .to(".right-leaf", { y: 200 }, 0)
       .to(".left-leaf", { y: -200 }, 0);
+
+    const startValue = isMobile ? "top 50%" : "center 60%";
+    const endValue = isMobile ? "120% top" : "bottom top";
+
+    let tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: "video",
+        start: startValue,
+        end: endValue,
+        scrub: true,
+        pin: true,
+      },
+    });
+
+    videoRef.current.onloadedmetadata = () => {
+      tl.to(videoRef.current, {
+        currentTime: videoRef.current.duration,
+      });
+    };
   }, []);
 
   return (
     <>
-      <section id="hero" className="noisy">
+      <section id="hero" className="noisy mb-80">
         <h1 className="title">MOJITO</h1>
 
         <img
@@ -56,8 +80,6 @@ const Hero = () => {
         />
 
         <div className="body">
-          {/* <img src="/images/arrow.png" alt="arrow" className="arrow" /> */}
-
           <div className="content">
             <div className="space-y-5 hidden md:block">
               <p>Cool. Crisp. Classic.</p>
@@ -79,13 +101,13 @@ const Hero = () => {
       </section>
 
       <div className="video absolute inset-0">
-        {/* <video
+        <video
           ref={videoRef}
           muted
           playsInline
           preload="auto"
           src="/videos/output.mp4"
-        /> */}
+        />
       </div>
     </>
   );
