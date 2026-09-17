829428c9c4f6703a2aeefbbfb485cac5d0158c03
IALikhon <likhon9112@gmail.com>
2026-09-17 19:37:33 +0600

    feat: add Cocktails component with GSAP animations and integrate into App

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -65,7 +65,7 @@ const Hero = () => {
 
   return (
     <>
-      <section id="hero" className="noisy mb-80">
+      <section id="hero" className="noisy">
         <h1 className="title">MOJITO</h1>
 
         <img
